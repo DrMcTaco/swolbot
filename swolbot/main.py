0d15ff69949c01eadb4b4be68c344c35e795e2f3
Dan Rubin <dmrubin3@gmail.com>
2020-11-20 21:25:08 -0500

    remove overload of on_message

--- swolbot/main.py
+++ swolbot/main.py
@@ -1,5 +1,7 @@
+from gc import isenabled
 import os
 from random import randint
+from re import U
 from typing import Optional
 
 import discord
@@ -9,7 +11,6 @@ from loguru import logger
 
 from swolbot.users import BrodinUser
 from swolbot.penance import Penance
-
 
 
 intents = discord.Intents.default()
@@ -27,27 +28,15 @@ async def on_ready():
     print(bot.user.id)
     print("------")
 
-
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("$hello"):
-        logger.info(message)
-        await message.channel.send(f"Hello {message.author}!")
-
-#TODO: Get penance to persist so status works
-@bot.group(name="swolbot", invoke_without_command=True)
-async def swolbot(ctx, user: discord.Member, action):
+@bot.group(name="swolbot", invoke_without_command=True, pass_context=True)
+async def swolbot(ctx, user: discord.Member, action: str):
     if ctx.invoked_subcommand is None:
         user = BrodinUser.get(user)
         if action.lower() == "died":
             task = Penance("pushups", reps=randint(5, 10))
             user.penance.append(task)
             await ctx.send(
-                f"{user.user.mention} {action}! Brodin demands you {task.demand}!"
+                f"{user.member.mention} {action}! Brodin demands you {task.demand}!"
             )
         else:
             task = Penance("plank", reps=randint(3, 6) * 10)
@@ -57,17 +46,18 @@ async def swolbot(ctx, user: discord.Member, action):
             )
 
 
-@swolbot.command(name="status")
+@swolbot.command(name="status", pass_context=True)
 async def _status(ctx, user: Optional[discord.Member]):
     if not user:
         user = ctx.message.author
     user = BrodinUser.get(user)
-    message = f"{user.user.mention} is in Brodin's good grace."
-    logger.info(user.penance)
+    message = f"{user.member.mention} is in Brodin's good grace."
     if user.unfinished_penance:
-        penance_str = ", ".join(user.unfinished_penance)
-        message = f"{user.user.mention} shall {penance_str} to please Brodin."
+        penance_str = ", ".join([task.demand for task in user.unfinished_penance])
+        message = f"{user.member.mention} shall {penance_str} to please Brodin."
     await ctx.send(message)
+    breakpoint()
+    logger.info(user.member.activities)
 
 
 load_dotenv()
